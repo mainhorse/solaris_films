1021250c9dfb943e5e24a57091189c9cad99c2f0
David Sanchez <cristiansanchezh@gmail.com>
2020-07-02 11:11:38 -0500

    [fix] cards cartelera funcionalidad [add] trailer
    Signed-off-by: David Sanchez <cristiansanchezh@gmail.com>

--- client/src/app/components/cartelera/cartelera.component.ts
+++ client/src/app/components/cartelera/cartelera.component.ts
@@ -10,6 +10,8 @@ export class CarteleraComponent implements OnInit {
   public card : String = 'card';
   public peliculas ;
   public url: String;
+  public cont = 0;
+  public video = 'video';
 
   constructor(private peliculaService : PeliculasService) { 
     this.url = peliculaService.url;
@@ -30,5 +32,23 @@ export class CarteleraComponent implements OnInit {
     })
   }
 
+  mostrar(posicion){
+    let pos = posicion.numPelicula;
+    let video="video";
+    let container = video + pos;
+    if(this.cont == 0){
+      document.getElementById(container).style.display = "block";
+      this.cont ++;
+    } else{
+      document.getElementById(container).style.display = "none";
+      this.cont = 0;
+    }
+    
+  }
+
+  comprar(datos){
+    alert(`La pelicula ${datos.titulo}, tiene un valor de $15.000 y una duración de ${datos.tiempo}`
+    )
+  }
 
 }
